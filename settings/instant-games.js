1f0b2e9eaf22d23f3c064e0350ef34f5baa51e23
Pursharth Zutshi <pursharthzutshi@gmail.com>
2020-11-21 18:41:04 +0530

    Add files via upload

---
+++ settings/instant-games.js
@@ -0,0 +1,11 @@
+let search = document.getElementById('search-input');
+
+
+function searchInput(search){
+    
+    search.style.color='red';
+    
+}
+
+
+search.addEventListener('click',searchInput);
